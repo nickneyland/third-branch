e491886d2a762c754ebbf2130669b292376fc4bb
Nick Neyland <nickneyland@gmail.com>
2026-02-27 12:31:17 -0600

    adding animation

--- src/js/aos.js
+++ src/js/aos.js
@@ -1,3 +1,14 @@
 import AOS from 'aos';
 
-AOS.init();
+// mobile detection
+const isMobile = () => {
+  return window.innerWidth <= 768 || 
+         /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+};
+
+AOS.init({
+  disable: isMobile,
+  // Additional options to ensure AOS is properly disabled on mobile
+  once: true,
+  duration: 600
+});
