817b2daa21a5e9cd04611e13650d86f116fd178e
Nick Neyland <nickneyland@gmail.com>
2026-03-10 10:20:48 -0400

    running new SEO fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://thirdbranchconsulting.com',
+  integrations: [sitemap(), robotsTxt()]
+});
